9b4f9a612d8471955439ecb08cfb29a1baad697a
Jamiel Sheikh <i@jamiel.net>
2021-03-11 01:20:55 -0500

    links

--- 14-buffett-indicator/run.py
+++ 14-buffett-indicator/run.py
@@ -1,14 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+
 def get_value(url,html_class,multiplier=1000000000):
     p = requests.get(url)
     soup = BeautifulSoup(p.content,'html.parser')
     r = soup.find_all('span', attrs={"class":html_class})
     return float(r[0].text.replace(',','')) * multiplier
 
+# https://www.frbatlanta.org/cqer/research/gdpnow/archives.aspx
+gdp_current_adjustment = 1.084 # Atlanta FRB
 
-gdp_current_adjustment = 1.084 # Atlanta FR
 gdp = get_value('https://fred.stlouisfed.org/series/GDP',
                 'series-meta-observation-value')
 gdp = gdp * gdp_current_adjustment
